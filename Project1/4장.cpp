70daa2a332859901eecb1f68b15397499db77a2e
신관우 <tlsrhksdn23@gmail.com>
2022-04-05 16:03:30 +0900

    .

--- Project1/4장.cpp
+++ Project1/4장.cpp
@@ -8,7 +8,7 @@
 
 //#include<iostream>
 //using namespace std;
-//
+// 
 //class Circle {
 //	int radius;
 //public:
@@ -34,7 +34,7 @@
 //	cout << p->getArea() << endl;  //donut의 getArea() 호출
 //	cout << (*p).getArea() << endl;  //donut의 getArea() 호출
 //
-//	p = &pizza;
+//	p = &pizza; 
 //	cout << p->getArea() << endl;   //pizza의 getArea() 호출
 //	cout << (*p).getArea() << endl; //pizza의 getArea() 호출
 //}
@@ -62,6 +62,109 @@
 //c[2]의 소멸자,c[1]의 소멸자, c[0]의 소멸자 실행
 
 //예쩨4-2.Circle 클래스의 배열 선언 및 활용
+//#include<iostream>
+//using namespace std;
+//
+//class Circle {
+//	int radius;
+//public:
+//	Circle() { radius = 1; }
+//	Circle(int r) { radius = r; }
+//	void setRadius(int r) { radius = r; }
+//	double getArea();
+//};
+//
+//double Circle::getArea() {
+//	return 3.14 * radius * radius;
+//}
+//
+//int main() {
+//	Circle circleArray[3];    //Circle 객체 배열 생성
+//
+//	//배열의 각 원소 객체의 멤버 접근
+//	circleArray[0].setRadius(10);
+//	circleArray[1].setRadius(20);
+//	circleArray[2].setRadius(30);
+//
+//	for (int i = 0;i < 3;i++)   //배열의 각 원소 객체의 멤버 접근 
+//		cout << "Circle " << i << "의 면적은 " << circleArray[i].getArea() << endl;
+//
+//	Circle * p;
+//	p = circleArray;
+//
+//	for (int i = 0;i < 3;i++) { //객체 포인터로 배열 접근
+//		cout << "Circle " << i << "의 면적은 " << p->getArea() << endl;
+//		p++;
+//	}
+//	
+//}
+
+//객체 배열 생성시 기본 생성자 호출
+//#include<iostream>
+//using namespace std;
+//
+//class Circle {
+//	int radius;
+//public:
+//	double getArea() {
+//		return 3.14 * radius * radius;
+//	}
+//};
+//
+//int main() {
+//	Circle circleArray[3];
+//}
+//
+//#include<iostream>
+//using namespace std;
+//
+//class Circle {
+//	int radius;
+//public:
+//	Circle(int r) { radius = r; }
+//	double getArea() {
+//		return 3.14 * radius * radius;
+//	}
+//};
+//
+//int main() {
+//	Circle waffle(15);
+//	Circle circleArray[3]; //기본 생성자 Circle() 호출, 기본 생성자가 없으므로 컴파일 오류
+//}
+
+//객체 배열 초기화
+//배열의 각 원소 객체당 생성자 지정하는 방법
+//Circle circleArray[3] = { Circle(10),Circle(20),Circle() };
+//circleArray[0] 객체가 생성될 때, 생성자 Circle(10) 호출
+//circleArray[1] 객체가 생성될 때, 생성자 Circle(20) 호출
+//circleArray[2] 객체가 생성될 때, 생성자 Circle() 호출
+
+//예제 4-3. 객체 배열 초기화
+//#include<iostream>
+//using namespace std;
+//
+//class Circle {
+//	int radius;
+//public:
+//	Circle() { radius = 1; }
+//	Circle(int r) { radius = r; }
+//	void setRadius(int r) { radius = r; }
+//	double getArea();
+//};
+//
+//double Circle::getArea() {
+//	return 3.14 * radius * radius;
+//}
+//
+//int main() {
+//	Circle circleArray[3] = { Circle(10),Circle(20),Circle() };  //Circle 배열 초기화
+//
+//	for (int i = 0;i < 3;i++) {
+//		cout << "Circle" << i << "의 면적은 " << circleArray[i].getArea() << endl;
+//	}
+//}
+
+//예제 4-4.Circle 클래스의 2차원 배열 선언 및 활용
 #include<iostream>
 using namespace std;
 
@@ -79,22 +182,18 @@ double Circle::getArea() {
 }
 
 int main() {
-	Circle circleArray[3];    //Circle 객체 배열 생성
+	Circle circle[2][3];
+	circle[0][0].setRadius(1);
+	circle[0][1].setRadius(2);
+	circle[0][2].setRadius(3);
+	circle[1][0].setRadius(4);
+	circle[1][1].setRadius(5);
+	circle[1][2].setRadius(6);
 
-	//배열의 각 원소 객체의 멤버 접근
-	circleArray[0].setRadius(10);
-	circleArray[1].setRadius(20);
-	circleArray[2].setRadius(30);
+	for(int i=0;i<2;i++)   //배열의 각 원소 객체의 멤버 접근
+		for (int j = 0;j < 3;j++) {
+			cout << "Circle [" << i <<"," << j << "]의 면적은 ";
+			cout << circle[i][j].getArea() << endl;
+		}
 
-	for (int i = 0;i < 3;i++)   //배열의 각 원소 객체의 멤버 접근 
-		cout << "Circle " << i << "의 면적은 " << circleArray[i].getArea() << endl;
-
-	Circle * p;
-	p = circleArray;
-
-	for (int i = 0;i < 3;i++) { //객체 포인터로 배열 접근
-		cout << "Circle " << i << "의 면적은 " << p->getArea() << endl;
-		p++;
-	}
-	
 }
